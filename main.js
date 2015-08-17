7ba2bf019729ba8d190b75992f05ebd0c1be7c2e
TakashiMatsuda <lobotomyplease@gmail.com>
2015-08-17 19:50:09 +0900

    delete test code

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
 ///////////////////////////////////////////////////////////
 // (c)2013 casley consulting inc, all right reserved.
-// サンプル
-// author y.Hata 2013/09/20
+// author t.matsuda 2015/08/17
 ///////////////////////////////////////////////////////////
- 
+
+//test
+/*
 document.OnLoad = start();
- 
-//////////////////////////////
-// ページ読込時の処理
-//////////////////////////////
 function start() {
     window.alert('Hello World !!');
 }
-
+*/
 //zizai_mask
 // ==UserScript==
 // @name         zizai_mask
