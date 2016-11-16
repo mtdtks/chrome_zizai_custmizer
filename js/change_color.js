138bcbdba9fad4dc53142a024c9b11386df36706
TakashiMatsuda <lobotomyplease@gmail.com>
2016-11-17 01:45:35 +0900

    fuck errorgit add js/change_color.js

--- js/change_color.js
+++ js/change_color.js
@@ -6,7 +6,8 @@ var chLabel = function (changeWord, newWord){
 };
 //.style.color = "red";
 var chColor = function (target){
-  document.getElementsByName(target).style.color = "red";
+  //document.getElementsByName(target).style.background = "#FF0000";
+  document.body.innerHTML = document.body.innerHTML.replace(target, target, 'g').style.color = "red";
 };
 
 //var target = [];
@@ -15,7 +16,7 @@ var chColor = function (target){
     //'著者'
 //];
 
-var targetText = "ISBN";
+//var targetText = "ISBN";
 
 //alert(REG.edit);
 var url = location.href;
@@ -28,7 +29,38 @@ if( REG.edit.test(url) ){
     chLabel(target[i]);
   }
   */
-  chLabel(targetText);
+  //var targetText = "ISBN";
+  //chColor(targetText);
+
+/*
+var cellVal = new Array;
+jQuery(function($) {
+  var tblTbody = document.getElementsByClassName('form');
+	// td内のtrをループ。rowsコレクションで,行位置取得。
+	for (var i=0, rowLen=tblTbody.rows.length; i<rowLen; i++) {
+		// tr内のtdをループ。cellsコレクションで行内セル位置取得。
+		for (var j=0, colLen=tblTbody.rows[i].cells.length ; j<colLen; j++) {
+		  //i行目のj列目のセルを取得
+		  var cells = tblTbody.rows[i].cells[j];
+		  //取得した値をテキストエリアへ表示
+		  //document.getElementById("txtArea_javascript_sample_table_1").value += i + "行目" + j + "列目の値は「" + cells.innerHTML + "」です。\n";
+      cellVal[i][j] = tblbody.rows[i].cells[j];
+      console.log(tblbody.rows[i].cells[j]);
+		}
+  }
+});
+*/
+/*
+var tText;
+tText = document.getElementsByClassName('form').table.rows[1].cells[0].firstChild.data;
+alert(tText);
+*/
+
+var alt;
+alt = document.getElementsByClassName("product_info").table;
+var txt;
+txt = alt.tbody.rows[1].cells[0].value;
+alert('txt');
 
 }
 
